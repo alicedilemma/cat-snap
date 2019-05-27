c3376282493e24d11a8205f9a588e199b5c4bba1
alicedilemma <unrelated@gmail.com>
2019-05-27 12:53:05 +1200

    Setup camera component CSS

--- client/components/CameraComponent.jsx
+++ client/components/CameraComponent.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Camera from 'react-html5-camera-photo'
-// import 'react-html5-camera-photo/build/css/index.css'
 
 class CameraContainer extends React.Component {
   onTakePhoto (dataUri) {
