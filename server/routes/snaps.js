a965126ca57da0f0ef7bd47a8bbada0983315d83
alicedilemma <unrelated@gmail.com>
2019-06-03 12:24:12 +1200

    switched formidable to formidable-express

--- server/routes/snaps.js
+++ server/routes/snaps.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const formidable = require('formidable')
-let fs = require('fs')
+const formidableMiddleware = require('express-formidable')
 
 const router = express.Router()
+router.use(formidableMiddleware())
 
 const db = require('../db/db')
 
@@ -18,28 +18,19 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-  const form = new formidable.IncomingForm()
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      console.error('Error', err)
-      throw err
-    }
-    console.log(files.image.path)
-    // const image = fs.readFileSync(files.file.File.path, { encoding: 'base64' })
-    // console.log('binary?', image)
-    // const { name, story, friendliness, lat, lng } = fields
-    // const snap = {
-    //   name,
-    //   story,
-    //   friendliness,
-    //   lat,
-    //   lng,
-    //   image
-    // }
-
-    // db.addSnap(req.body)
-    //   .then(() => res.send('added!'))
-  })
+  // Formidable form sends data in req.fields and req.files
+  const { name, story, friendliness, lat, lng, image } = req.fields
+  const snap = {
+    name,
+    story,
+    friendliness,
+    lat,
+    lng,
+    image
+  }
+  db.addSnap(snap)
+    .then(() => { res.send('ok') })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
